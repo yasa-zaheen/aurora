c510c1f92df6986d66f400c108fb5a9144945558
yasa-zaheen <yasazaheen@gmail.com>
2021-03-29 12:18:11 +0600

    product card url linking

--- main/static/main/js/products.js
+++ main/static/main/js/products.js
@@ -1,3 +1,16 @@
+const imageResizer = () => {
+  const visImgs = document.querySelectorAll("img.vis");
+
+  visImgs.forEach((visImg) => {
+    if (visImg.clientWidth > visImg.clientHeight) {
+      visImg.style.width = "auto";
+      visImg.style.height = "100%";
+    }
+  });
+};
+
+imageResizer();
+
 const prodCardBtnAnimations = (buttons, color) => {
   buttons.forEach((button) => {
     button.addEventListener("click", function () {
