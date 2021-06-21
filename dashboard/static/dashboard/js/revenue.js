7a6c6a23bd30187c6d26a263b7a3a92784869d62
yasa-zaheen <yasazaheen@gmail.com>
2021-06-21 11:37:43 +0600

    Revenue Traffic Frontend

--- dashboard/static/dashboard/js/revenue.js
+++ dashboard/static/dashboard/js/revenue.js
@@ -1,49 +1,101 @@
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00"],
-    datasets: [
-      {
-        label: "Revenue",
-        data: [6, 2, 8, 7, 3, 10],
-        backgroundColor: "#ee6c4d",
-        borderRadius: 15,
-        barThickness: 15,
-      },
-      {
-        label: "Sales",
-        data: [6, 2, 8, 7, 3, 10],
-        backgroundColor: "#293241",
-        borderRadius: 15,
-        barThickness: 15,
-      },
-    ],
-  },
-  options: {
-    plugins: {
-      legend: {
-        display: false,
-      },
+const revenueSalesGraph = () => {
+  let ctx = document.getElementById("revenueSales__graph").getContext("2d");
+  let myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00"],
+      datasets: [
+        {
+          label: "Revenue",
+          data: [6, 2, 8, 7, 3, 10],
+          backgroundColor: "#ee6c4d",
+          borderRadius: 15,
+          barThickness: 15,
+        },
+        {
+          label: "Sales",
+          data: [6, 2, 8, 7, 3, 10],
+          backgroundColor: "#293241",
+          borderRadius: 15,
+          barThickness: 15,
+        },
+      ],
     },
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      x: {
-        grid: {
+    options: {
+      plugins: {
+        legend: {
           display: false,
         },
       },
-      y: {
-        grid: {
-          display: true,
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          grid: {
+            display: false,
+          },
         },
-        display: false,
+        y: {
+          grid: {
+            display: true,
+          },
+          display: false,
+        },
       },
     },
-  },
-});
+  });
 
-Chart.defaults.font.family = "Poppins";
-Chart.defaults.font.weight = "600";
-Chart.defaults.font.size = 12.5;
+  Chart.defaults.font.family = "Poppins";
+  Chart.defaults.font.weight = "600";
+  Chart.defaults.font.size = 12.5;
+};
+
+revenueSalesGraph();
+
+const trafficGraph = () => {
+  let ctx = document.getElementById("traffic__graph").getContext("2d");
+  let myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00"],
+      datasets: [
+        {
+          label: "Product Views",
+          data: [6984, 2234, 8098, 7092, 3142, 10120],
+          backgroundColor: "#ee6c4d",
+          borderColor: "#ee6c4d2f",
+        },
+        {
+          label: "Unique Viewers",
+          data: [152, 235, 481, 326, 911],
+          backgroundColor: "#293241",
+          borderColor: "#2932412f",
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          grid: {
+            display: false,
+          },
+        },
+        y: {
+          grid: {
+            display: true,
+          },
+          display: false,
+        },
+      },
+    },
+  });
+};
+
+trafficGraph();
