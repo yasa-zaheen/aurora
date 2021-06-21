bc2065c50815f8fb5a881e4ab20627b2911cf409
yasa-zaheen <yasazaheen@gmail.com>
2021-06-21 12:48:16 +0600

    Revenue front-end

--- dashboard/static/dashboard/js/revenue.js
+++ dashboard/static/dashboard/js/revenue.js
@@ -99,3 +99,30 @@ const trafficGraph = () => {
 };
 
 trafficGraph();
+
+const crmCrmStatusButtonChangeUI = () => {
+  const crmCrmStatusButtons = document.querySelectorAll(
+    "button.table__statusBtn"
+  );
+
+  crmCrmStatusButtons.forEach((crmCrmStatusButton) => {
+    crmCrmStatusButton.style.color = "#2f2f2f";
+
+    switch (crmCrmStatusButton.getAttribute("data-status")) {
+      case "pending":
+        crmCrmStatusButton.style.backgroundColor = "#ef233c2f";
+        crmCrmStatusButton.innerText = "Pending";
+        break;
+      case "delivering":
+        crmCrmStatusButton.style.backgroundColor = "#2f2f2f2f";
+        crmCrmStatusButton.innerText = "Delivering";
+        break;
+      case "delivered":
+        crmCrmStatusButton.style.backgroundColor = "#2932412f";
+        crmCrmStatusButton.innerText = "Delivered";
+        break;
+    }
+  });
+};
+
+crmCrmStatusButtonChangeUI();
