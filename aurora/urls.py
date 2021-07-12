58d019f7c9630e9fa5daf579e2ca864fc1b15aa2
yasa-zaheen <yasazaheen@gmail.com>
2021-07-12 12:49:57 +0600

    THEAUR-16: Sign in page

--- aurora/urls.py
+++ aurora/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('', include('authentication.urls')),
+    path('auth/', include('authentication.urls')),
     path('', include('information.urls')),
     path('dashboard/', include('dashboard.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
